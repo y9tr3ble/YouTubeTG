5fe3627c70c240d7a3b644a1b3bf72c6e33e1110
y9tr3ble <tr3ble@outlook.com>
2025-01-12 10:30:50 +0300

    moved the search command to a different directory

--- src/bot.ts
+++ src/bot.ts
@@ -1,30 +1,9 @@
-import { Bot, InlineKeyboard } from "grammy";
+import { Bot } from "grammy";
 import { botToken } from './config';
-import { searchVideos } from './services/searchVideo';
-import { YoutubeResult } from './interfaces/youtube';
+import { searchCommand } from './commands/searchCommand';
 
 const bot = new Bot(botToken);
 
-bot.command("search", async (ctx) => {
-    const query = "deadp47";
-    try {
-        const result = await searchVideos(query);
-
-        result.forEach((video: YoutubeResult) => {
-            const videoTitle = video.title;
-            const channelName = video.channelTitle;
-            console.log(`Video Title: ${videoTitle}, Channel: ${channelName}`);
-            const inlineKeyboard = new InlineKeyboard()
-                .text("Скачать MP3🎵", "mp3")
-                .text("Скачать MP4🎥", "mp4");
-            ctx.reply(`Video: ${videoTitle}\nChannel: ${channelName}`, {
-                reply_markup: inlineKeyboard,
-            });
-        });
-    } catch (error) {
-        console.error('Error searching video:', error);
-        await ctx.reply('An error occurred while searching for videos.');
-    }
-});
+bot.command("search", searchCommand);
 
 bot.start();
---
+++ src/commands/searchCommand.ts
@@ -0,0 +1,28 @@
+import { Context } from "grammy";
+import { searchVideos } from '../services/searchVideo';
+import { YoutubeResult } from '../interfaces/youtube';
+import { InlineKeyboard } from 'grammy';
+
+export const searchCommand = async (ctx: Context) => {
+    const query = "deadp47";
+    try {
+        const result = await searchVideos(query);
+
+        result.forEach((video: YoutubeResult) => {
+            const videoTitle = video.title;
+            const channelName = video.channelTitle;
+            console.log(`Video Title: ${videoTitle}, Channel: ${channelName}`);
+
+            const inlineKeyboard = new InlineKeyboard()
+                .text("Скачать MP3🎵", "mp3")
+                .text("Скачать MP4🎥", "mp4");
+
+            ctx.reply(`Video: ${videoTitle}\nChannel: ${channelName}`, {
+                reply_markup: inlineKeyboard,
+            });
+        });
+    } catch (error) {
+        console.error('Error searching video:', error);
+        await ctx.reply('An error occurred while searching for videos.');
+    }
+};
